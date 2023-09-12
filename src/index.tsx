c5448a31976a5c4c01fbf0d231b03e4aba27e8e8
Namhyeon Heo <nammath11@gmail.com>
2023-09-13 00:09:48 +0900

    change pagination

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider as MuiThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { BrowserRouter, RouterProvider } from "react-router-dom";
 import router from "./router";
 import theme from "./theme";
@@ -9,16 +11,21 @@ import "./fonts.css";
 
 const client = new QueryClient();
 
+const muiTheme = createTheme();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <RouterProvider router={router}></RouterProvider>
-      </ChakraProvider>
+      <MuiThemeProvider theme={muiTheme}>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <RouterProvider router={router}></RouterProvider>
+          {/* Your main application component */}
+        </ChakraProvider>
+      </MuiThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
